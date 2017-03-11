ad3b37060159ed4d0c2e24ecd081de1db73ebdd0
Alhadis <gardnerjohng@gmail.com>
2017-03-11 22:10:19 +1100

    Add fledgeling test-suite
    I'll probably fill these out another time... holy crap, this takes ages.

--- src/utils/other.js
+++ src/utils/other.js
@@ -2,44 +2,86 @@
 
 const {exec} = require("child_process");
 
-
 module.exports = {
-
-	/**
-	 * Round off a fractional value using arbitrary precision.
-	 *
-	 * @param {Number} value
-	 * @param {Number} [precision = 0]
-	 * @return {Number}
-	 */
-	round(value, precision = 0){
-		const factor = Math.pow(10, precision);
-		return Math.round(value * factor) / factor;
-	},
-	
-	
-	/**
-	 * Return the number of digits after a value's decimal point.
-	 *
-	 * @example getPrecision(8.23); => 2
-	 * @param {Number} value
-	 * @return {Number}
-	 */
-	getPrecision(value){
-		return /\./.test(value)
-			? value.toString().split(".").slice(1).join("").length
-			: 0;
-	},
-	
-	
-	/**
-	 * Execute a shell-command in a child process.
-	 *
-	 * @param {String} command
-	 * @return {ChildProcess}
-	 * @see {@link https://nodejs.org/api/child_process.html}
-	 */
-	shell(command){
-		return exec(command);
-	}
+	round,
+	getPrecision,
+	wait,
+	setTheme,
+	shell
 };
+
+
+/**
+ * Round off a fractional value using arbitrary precision.
+ *
+ * @param {Number} value
+ * @param {Number} [precision = 0]
+ * @return {Number}
+ */
+function round(value, precision = 0){
+	const factor = Math.pow(10, precision);
+	return Math.round(value * factor) / factor;
+}
+
+
+/**
+ * Return the number of digits after a value's decimal point.
+ *
+ * @example getPrecision(8.23); => 2
+ * @param {Number} value
+ * @return {Number}
+ */
+function getPrecision(value){
+	return /\./.test(value)
+		? value.toString().split(".").slice(1).join("").length
+		: 0;
+}
+
+
+/**
+ * Return a {@link Promise} that resolves after a delay.
+ *
+ * @param {Number} [delay=100] - Milliseconds to wait
+ * @return {Promise}
+ */
+function wait(delay = 100){
+	return new Promise(resolve => {
+		setTimeout(() => resolve(), delay);
+	});
+}
+
+
+/**
+ * Change the active Atom themes.
+ *
+ * @example setTheme("one-dark").then(() => …);
+ * @param {...String} names - Theme IDs, sans suffix.
+ * @return {Promise}
+ */
+function setTheme(...names){
+	const [ui, syntax] = names.length < 2
+		? [`${names[0]}-ui`, `${names[0]}-syntax`]
+		: names;
+	
+	return Promise.all([
+		atom.packages.activatePackage(ui),
+		atom.packages.activatePackage(syntax)
+	]).then(() => {
+		atom.config.set("core.themes", [ui, syntax]);
+		atom.themes.addActiveThemeClasses();
+		atom.themes.loadBaseStylesheets();
+		atom.themes.emitter.emit("did-change-active-themes");
+	}).then(() => wait(500));
+}
+
+
+/**
+ * Execute a shell-command in a child process.
+ *
+ * @param {String} command
+ * @return {ChildProcess}
+ * @see {@link https://nodejs.org/api/child_process.html}
+ */
+function shell(command){
+	return exec(command);
+}
