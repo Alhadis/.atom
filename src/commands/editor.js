Alhadis/.atom

commit 8cfd842311ae4e72d31390d6f260504b56d9f69c

Block brace-balancing when typing [ after ANSI escape

src/commands/editor.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const {round, getPrecision, tsvTable} = require("../utils/other.js");
-const {hasSelectedText, mutate} = require("../utils/buffer.js");
+const {hasSelectedText, mutate, surround} = require("../utils/buffer.js");
 const EDITOR_PANES = "atom-text-editor:not([mini])";
 
 
@@ -193,6 +193,26 @@ atom.commands.add(EDITOR_PANES, "editor:expand-escapes", event => {
 });
 
 
+// Suppress auto-insertion of closing bracket when necessary
+atom.commands.add(EDITOR_PANES, "user:square-bracket", event => {
+	const editor = atom.workspace.getActiveTextEditor();
+	const {buffer} = editor;
+	for(const selection of editor.getSelectionsOrderedByBufferPosition()){
+		if(selection.isEmpty()){
+			const range = selection.getBufferRange();
+			const {start} = range;
+			const {row}   = start;
+			const before  = buffer.getTextInRange([[row, 0], start]);
+			const after   = buffer.getTextInRange([start, buffer.rangeForRow(row).end]);
+			editor.buffer.insert(start, shouldBalance("[", before, after) ? "[]" : "[");
+			const point = start.translate([0, 1]);
+			selection.setBufferRange([point, point]);
+		}
+		else surround("[", "]", selection);
+	}
+});
+
+
 // Replace Atom's built-in uppercase/lowercase commands with ones that won't auto-select
 for(const commandName of ["editor:upper-case", "editor:lower-case"]){
 	const handler = /upper/.test(commandName)
@@ -220,6 +240,17 @@ function bumpSelectedNumbers(by = 1, editor = null){
 		});
 		selection.insertText(text, {select: true});
 	}, 150);
+}
+
+
+function shouldBalance(char, textBefore, textAfter){
+	switch(char){
+		case "[":
+			if(/(?:\\x1B|\\033|\x1B)$/.test(textBefore))
+				return false;
+		default:
+			return true;
+	}
 }
 
 

src/configs/keymap.cson
@@ -62,6 +62,7 @@
 	# "cmd-'":        null # self they're available.
 	"cmd-0":          "user:zoom-text"
 	"shift-cmd-e":    "user:specsaver"
+	"[":              "user:square-bracket"
 	"cmd-r":          "unset!"
 	"cmd-=":          "user:increment"
 	"cmd-+":          "user:increment"

src/utils/buffer.js
@@ -72,6 +72,28 @@ module.exports = {
 
 
 	/**
+	 * Enclose a text selection with two strings.
+	 *
+	 * @param {String} before
+	 * @param {String} after
+	 * @param {Selection} [target=null]
+	 */
+	surround(before, after, target = null){
+		before = String(before);
+		after  = String(after);
+		target = target || atom.workspace.getActiveTextEditor().getLastSelection();
+		const {editor} = target;
+		return editor.transact(100, () => {
+			const range = target.getBufferRange();
+			const start = range.start.translate([0, before.length]);
+			const end   = range.toDelta().row ? range.end : range.end.translate([0, before.length]);
+			target.insertText(before + target.getText() + after, {normalizeLineEndings: false});
+			target.setBufferRange([start, end]);
+		});
+	},
+	
+
+	/**
 	 * Report whether a {@link TextEditor} contains selected text.
 	 *
 	 * @param {TextEditor} editor

commit e3d9ba00101fdeeff6fc7ea35e523f8d1e7193c6

Use native insertion methods to balance brackets
Atom was no longer moving the cursor past the closing `]` after a square
bracket was balanced. E.g., typing `]` after `[` resulted in `[]]`. It's
unknown if this behaviour is exposed by an API.

Regression caused by 8cfd842.

src/commands/editor.js
@@ -204,9 +204,13 @@ atom.commands.add(EDITOR_PANES, "user:square-bracket", event => {
 			const {row}   = start;
 			const before  = buffer.getTextInRange([[row, 0], start]);
 			const after   = buffer.getTextInRange([start, buffer.rangeForRow(row).end]);
-			editor.buffer.insert(start, shouldBalance("[", before, after) ? "[]" : "[");
-			const point = start.translate([0, 1]);
-			selection.setBufferRange([point, point]);
+			if(shouldBalance("[", before, after))
+				editor.insertText("[");
+			else{
+				editor.buffer.insert(start, "[");
+				const point = start.translate([0, 1]);
+				selection.setBufferRange([point, point]);
+			}
 		}
 		else surround("[", "]", selection);
 	}